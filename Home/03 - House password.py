20fe8915c8da2c42d1d2b5366431731d0ba36b25
attillax <attillax.lab@gmail.com>
2014-07-05 01:57:49 +0200

    Solution added

--- Home/03 - House password.py
+++ Home/03 - House password.py
@@ -1,11 +1,16 @@
 def checkio(data):
-
-    #replace this for solution
-    return True or False
-
-#Some hints
-#Just check all conditions
-
+    counter = ''
+    for i in range(len(data)):
+        if data[i] in 'abcdefghijklmnopqrstuvwxyz':
+            counter += 'a'
+        if data[i] in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            counter += 'b'
+        if data[i] in '0123456789':
+            counter += 'c'
+    if ((('a' in counter) and ('b' in counter)) and (('c' in counter) and (len(data) >= 10))):
+        return True
+    else:
+        return False
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
@@ -14,4 +19,4 @@ if __name__ == '__main__':
     assert checkio(u'asasasasasasasaas') == False, "3rd example"
     assert checkio(u'QWERTYqwerty') == False, "4th example"
     assert checkio(u'123456123456') == False, "5th example"
-    assert checkio(u'QwErTy911poqqqq') == True, "6th example"
+    assert checkio(u'QwErTy911poqqqq') == True, "6th example" 
