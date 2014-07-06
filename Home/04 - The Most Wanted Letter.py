8b0fa58fadfec9c44ed36d4fa87cd2808937da85
attillax <attillax.lab@gmail.com>
2014-07-06 03:45:52 +0200

    Solution added

--- Home/04 - The Most Wanted Letter.py
+++ Home/04 - The Most Wanted Letter.py
@@ -1,7 +1,20 @@
+# -*- coding: utf-8 -*-
 def checkio(text):
+    dict1 = {'a': 0,'b': 0,'c': 0,'d': 0,'e': 0,'f': 0,'g': 0,'h': 0,'i': 0,'j': 0,'k': 0,'l': 0,'m': 0,'n': 0,'o': 0,'p': 0,'q': 0,'r': 0,'s': 0,'t': 0,'u': 0,'v': 0,'w': 0,'x': 0,'y': 0,'z': 0}
+    for i in range(len(text)):
+        a = text[i]
+        if a in 'abcdefghijklmnopqrstuvwxyz':
+            dict1[a] += 1
+        if a in 'ABCDEFGHIJKLMNOPQRSTUVWXYZ':
+            b = a.lower()
+            dict1[b] += 1
+    d = max(dict1.values())
 
-    #replace this for solution
-    return 'a'
+    for key, value in dict1.items():
+        if value == d:
+                res = key
+                break
+    return res 
 
 if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
