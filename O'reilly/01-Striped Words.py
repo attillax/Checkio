c1d665f93433e351898e3c0ab3df730cfe7987ab
attillax <attillax.lab@gmail.com>
2014-07-10 03:11:25 +0200

    Added final solution

--- O'reilly/01-Striped Words.py
+++ O'reilly/01-Striped Words.py
@@ -1,36 +1,48 @@
 VOWELS = "AEIOUY"
 CONSONANTS = "BCDFGHJKLMNPQRSTVWXZ"
+c = '0123456789'
 a = VOWELS.lower()
 b = CONSONANTS.lower()
+d = a+b+c
 
-def slice(text):
+def checkio(text):
     tt = text.lower()
     words = []
     start = 0
     for i in range(len(tt)):
-        if (not((tt[i] in a) or (tt[i] in b)) and ((tt[i-1] in a) or (tt[i-1] in b))):
+        if (not(tt[i] in d) and (tt[i-1] in d)):
             end = i
             words.append(tt[start:end])
-        elif ((i == (len(tt)-1) and ((tt[i-1] in a) or (tt[i-1] in b)))):
+        elif (i == (len(tt)-1) and (tt[i-1] in d)):
             words.append(tt[start:len(tt)])
-        if (not((tt[i-1] in a) or (tt[i-1] in b)) and ((tt[i] in a) or (tt[i] in b))):
+        if (not(tt[i-1] in d) and (tt[i] in d)):
             start = i
-    return (words)
+    wt = []
+    for w in words:
+        flag = 0
+        for i in w:
+            if i in c:
+                flag = 1
+        if flag == 0:
+            wt.append(w)
+    res = 0
+    for w in wt:
+        check = len(w)*'vc'
+        count = ''
+        for i in w:
+            if i in a:
+                count += 'v'
+            if i in b:
+                count += 'c'
+        if (count in check) and (len(w) > 1):
+            res += 1
+    return res
 
-def checkio(text):
-    words = slice(text)
-    print words
-
-'''
-#These "asserts" using only for self-checking and not necessary for auto-testing
+'''#These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     assert checkio(u"My name is ...") == 3, "All words are striped"
     assert checkio(u"Hello world") == 0, "No one"
     assert checkio(u"A quantity of striped words.") == 1, "Only of"
-    assert checkio(u"Dog,cat,mouse,bird.Human.") == 3, "Dog, cat and human" '''
+    assert checkio(u"Dog,cat,mouse,bird.Human.") == 3, "Dog, cat and human"'''
 
-checkio("My name is ...")
-checkio("Hello world")
-checkio("A quantity of striped words.")
-checkio("Dog,cat,mouse,bird.Human.")
-checkio("dfghj werty ertyui werty")
+checkio("1st 2a ab3er root rate")
