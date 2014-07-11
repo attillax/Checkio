b7ed8336a7eb91d77c032001a7dac5b617e5825f
attillax <attillax.lab@gmail.com>
2014-07-11 21:33:53 +0200

    Added solution

--- ScientificExpedition/01-VerifyAnagrams.py
+++ ScientificExpedition/01-VerifyAnagrams.py
@@ -1,6 +1,17 @@
-def verify_anagrams(first_word, second_word):
+def make_list(word):
+    res = []
+    lowstr = 'abcdefghijklmnopqrstuvwxyz'
+    temp = word.lower()
+    for t in temp:
+        if t in lowstr:
+            res.append(t)
+    a = sorted(res)
+    return a
 
-    return True or False
+def verify_anagrams(first_word, second_word):
+    w1 = make_list(first_word)
+    w2 = make_list(second_word)
+    return w1 == w2
 
 '''if __name__ == '__main__':
     #These "asserts" using only for self-checking and not necessary for auto-testing
@@ -8,6 +19,3 @@ def verify_anagrams(first_word, second_word):
     assert verify_anagrams(u"Programming", u"Gram Ring Mop") == True, "Gram of code"
     assert verify_anagrams(u"Hello", u"Ole Oh") == False, "Hello! Ole Oh!"
     assert verify_anagrams(u"Kyoto", u"Tokyo") == True, "The global warming crisis of 3002" '''
-
-
-
