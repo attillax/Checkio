d03d4bf6e0e7ec5d96a5c1e88b656c7ddc8d50f2
attillax <attillax.lab@gmail.com>
2014-07-12 13:52:37 +0200

    Added solution

--- Library/01-FizzBuzz.py
+++ Library/01-FizzBuzz.py
@@ -1,21 +1,23 @@
-#Your optional code here
-#You can import some modules or create additional functions
-
 
 def checkio(number):
-    #Your code here
-    #It's main function. Don't remove this function
-    #It's using for auto-testing and must return a result for check.
+    if number%3 == 0 :
+        res = 'Fizz'
+        if number%5 == 0:
+            res += 'Buzz'
+    elif number%5 == 0:
+        res = 'Buzz'
+    else:
+        res = str(number)
+    return res
 
-    #replace this for solution
-    return str(number)
-
-#Some hints:
-#Convert a number in the string with str(n)
-
-#These "asserts" using only for self-checking and not necessary for auto-testing
+'''#These "asserts" using only for self-checking and not necessary for auto-testing
 if __name__ == '__main__':
     assert checkio(15) == "Fizz Buzz", "15 is divisible by 3 and 5"
     assert checkio(6) == "Fizz", "6 is divisible by 3"
     assert checkio(5) == "Buzz", "5 is divisible by 5"
-    assert checkio(7) == "7", "7 is not divisible by 3 or 5"
+    assert checkio(7) == "7", "7 is not divisible by 3 or 5"'''
+
+print checkio(15) # "Fizz Buzz"
+print checkio(6) # "Fizz"
+print checkio(5) # "Buzz"
+print checkio(7) # "7"
